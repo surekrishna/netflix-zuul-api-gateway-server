cf1ec1d341c5674eddf2cb12f16bc53cdd669ddf
Krishna <surekrrishna@gmail.com>
2018-09-19 14:21:22 +0530

    Implementing Zuul Loggining Filter

---
+++ src/main/java/com/spring/boot/microservices/netflixeurekanamingserver/netflixzuulapigatewayserver/ZuulLoggingFilter.java
@@ -0,0 +1,41 @@
+package com.spring.boot.microservices.netflixeurekanamingserver.netflixzuulapigatewayserver;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+	
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		LOGGER.info("Request -> {} Request URI -> {} ", request, request.getRequestURI());
+		
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "error";
+	}
+
+	@Override
+	public int filterOrder() {		
+		return 1;
+	}
+
+}
